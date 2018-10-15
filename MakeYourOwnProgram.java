817bf6eb768c1b283c24effb1e84b641aefa96b5
Ben B <lerminator3@gmail.com>
2018-10-15 15:49:18 -0400

    Make your own working program here that works with two wheels and an arm.

---
+++ MakeYourOwnProgram.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="TankDrive", group="Linear Opmode")
+@Disabled
+public class TankDrive extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() { // Start the code ("INIT" is pressed)
+        telemetry.addData("Status", "Initialized");// Show text on the phone that we've pressed the "INIT" button
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+        // Show the elapsed game time at the end of the match
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
+    }
+}
