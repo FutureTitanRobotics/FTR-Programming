1edc0514f6fe20ab950c36bc2ef6e1596fd8fd9f
Jacob C <43409401+AnorakTheAwesome@users.noreply.github.com>
2018-11-01 12:03:49 -0400

    Create Servo Test 1
    The first test to see if the servo can hold the weight of the arm. There is still a chance that the code is wrong and the servo CAN hold the weight of the arm. Give it a chance

---
+++ ServoTest1.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import
+
+@TeleOp(name"Servo Test 1", group="Linear Opmode")
+@Disabled
+public class "Servo Test 1" extends LinearOpMode { 
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode() { // Start the code ("INIT" is pressed)
+        telemetry.addData("Status", "Initialized");// Show text on the phone that we've pressed the "INIT" button
+        telemetry.update();
+        
+        
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+        // Show the elapsed game time at the end of the match
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
+    }
+}
