b1da30b01b8932e4ac496578d07c458b27d10076
Jacob C <43409401+AnorakTheAwesome@users.noreply.github.com>
2018-11-05 11:51:31 -0500

    Update ServoTest1.java

--- ServoTest1.java
+++ ServoTest1.java
@@ -15,12 +15,21 @@ public class "Servo Test 1" extends LinearOpMode {
 
     DcMotor lmotor;
     DcMotor rmotor;
-
+    Servo armservo;
+    
     @Override
     public void runOpMode() { // Start the code ("INIT" is pressed)
         telemetry.addData("Status", "Initialized");// Show text on the phone that we've pressed the "INIT" button
         telemetry.update();
         
+        lmotor = hardwareMap.get(DcMotor.class, "left_drive");
+        rmotor = hardwareMap.get(DcMotor.class, "right_drive");
+        armservo = hardwareMap.servo.get("armservo");
+        
+        double position = 1;
+        double servopos = 1;
+        
+         lmotor.setDirection(DcMotor.Direction.REVERSE);
         
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -28,6 +37,29 @@ public class "Servo Test 1" extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            //Make forward and backward movement the up and down on the right joystick of the controller.
+            double left = gamepad1.left_stick_y;
+            //Turn or rotate with the left joystick
+            double right = gamepad1.right_stick_y;
+            
+            if (left < 0.1 || left > -0.1) {//set the left motor to the power of the left joystick
+                lmotor.setPower(left);
+            }
+            if (right < 0.1 || right > -0.1) {//set the right motor to the power of the righ joystick
+                rmotor.setPower(right);
+            }
+            else {
+                lmotor.setPower(0); // If the joysticks are not being moved, stop all motors
+                rmotor.setPower(0);
+            }
+            
+            else if (gamepad2.b) {
+                armservo.setPosition(10)
+            }
+            else if (!gamepad2.a && !gamepad2.b) {
+                armservo.setPosition(0)
+            }
+        }
 
         // Show the elapsed game time at the end of the match
         telemetry.addData("Status", "Run Time: " + runtime.toString());
